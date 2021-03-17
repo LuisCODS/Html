d6555f77cdf3b7351aadaaf44cdd1a5f176e938e
Luis <tennis.bresil@gmail.com>
2021-03-17 08:30:42 -0400

    a
    a

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 
 
+// let elt = document.getElementById('main');
+// elt.innerHTML = "<ul><li>Elément 1</li><li>Elément 2</li></ul>";
+
+// console.log(elt);
+
+elt = document.getElementById('test')
+elt.innerHTML = "content";
+elt.style.color = "red";      // Change la couleur du texte de l'élément à blanche
+elt.style.backgroundColor = "#000"; // Change la couleur de fond de l'élément en noir
+elt.style.fontWeight = "bold"; // Met le texte de l'élément en gras
+
+console.log("elt");
+
+
+// const contents = document.getElementsByClassName('content');
+// const firstContent = contents[0].innerText = "Paragraph changed!";
 
